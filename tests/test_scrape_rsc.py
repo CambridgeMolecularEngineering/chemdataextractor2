c178fbdf9eb8eb148d0ebee34525ff26de61dee5
Taketomo <ti250@cam.ac.uk>
2018-10-18 10:12:08 +0100

    Add tests for RscSearchScraper

--- tests/test_scrape_rsc.py
+++ tests/test_scrape_rsc.py
@@ -14,7 +14,7 @@ from __future__ import unicode_literals
 import logging
 import unittest
 
-from chemdataextractor.scrape.pub.rsc import rsc_substitute, strip_rsc_html
+from chemdataextractor.scrape.pub.rsc import rsc_substitute, strip_rsc_html, RscSearchScraper
 
 
 logging.basicConfig(level=logging.DEBUG)
@@ -47,3 +47,35 @@ class TestStripRscHtml(unittest.TestCase):
         html = '<span class="title_heading">Rationale for the sluggish oxidative addition of aryl halides to Au(<span class="small_caps">I</span>)<a title="Electronic supplementary information (ESI) available. CCDC 891201–891204 and 964933. For ESI and crystallographic data in CIF or other electronic format see DOI: 10.1039/c3cc48914k" href="#fn1">†</a></span>'
         stripped = '<span class="title_heading">Rationale for the sluggish oxidative addition of aryl halides to Au(I)</span>'
         self.assertEqual(strip_rsc_html.clean_html(html), stripped)
+
+class TestRscSearchScraper(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        query_text = "Aspirin"
+        scraper = RscSearchScraper()
+        scrape = scraper.run(query_text)
+        scrape_10 = scraper.run(query_text, 10)
+        self.results = scrape.serialize()
+        self.results_10 = scrape_10.serialize()
+
+    def test_doi(self):
+        self.assertTrue('doi' in self.results[0].keys())
+
+    def test_title(self):
+        self.assertTrue('title' in self.results[0].keys())
+
+    def test_landing_url(self):
+        self.assertTrue('landing_url' in self.results[0].keys())
+
+    def test_pdf_url(self):
+        self.assertTrue('pdf_url' in self.results[0].keys())
+
+    def test_html_url(self):
+        self.assertTrue('html_url' in self.results[0].keys())
+
+    def test_journal(self):
+        self.assertTrue('journal' in self.results[0].keys())
+
+    def test_pagination(self):
+        self.assertNotEqual(self.results[0], self.results_10[0])
