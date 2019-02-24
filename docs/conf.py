3abf06390ae7f2216ca1a26b0efe6b1bc11db298
Taketomo <ti250@cam.ac.uk>
2019-02-24 23:55:25 +0000

    Add support for numpy/google-style docstrings.

--- docs/conf.py
+++ docs/conf.py
@@ -49,11 +49,11 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     'sphinx.ext.autosummary',
+    'sphinx.ext.napoleon',
     'm2r',
     'nbsphinx',
     'nbsphinx_link',
 ]
-
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
