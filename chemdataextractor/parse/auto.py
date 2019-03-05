17202683382fbca353a41ff11a1b00ce144c2fab
Taketomo <ti250@cam.ac.uk>
2019-03-05 21:16:44 +0000

    Remove 'value_phrase'

--- chemdataextractor/parse/auto.py
+++ chemdataextractor/parse/auto.py
@@ -103,7 +103,7 @@ class BaseAutoParser(BaseParser):
         if hasattr(self.model, 'dimensions') and not self.model.dimensions:
             # the mandatory elements of Dimensionless model are grouped into a entities list
             specifier = self.model.specifier.parse_expression('specifier')
-            value_phrase = value_element_plain()('value_phrase')
+            value_phrase = value_element_plain()
             entities.append(specifier)
             entities.append(value_phrase)
 
@@ -111,11 +111,10 @@ class BaseAutoParser(BaseParser):
             # the mandatory elements of Quantity model are grouped into a entities list
             # print(self.model, self.model.dimensions)
             unit_element = Group(
-                construct_unit_element(self.model.dimensions).with_condition(match_dimensions_of(self.model))('units'))(
-                'value_phrase')
+                construct_unit_element(self.model.dimensions).with_condition(match_dimensions_of(self.model))('units'))
             specifier = self.model.specifier.parse_expression('specifier') + Optional(lbrct) + Optional(W('/')) + Optional(
                 unit_element) + Optional(rbrct)
-            value_phrase = (value_element_plain()('value_phrase') + Optional(unit_element))
+            value_phrase = (value_element_plain() + Optional(unit_element))
             entities.append(specifier)
             entities.append(value_phrase)
 
@@ -151,7 +150,7 @@ class BaseAutoParser(BaseParser):
 
         if hasattr(self.model, 'dimensions') and not self.model.dimensions:
             # the specific entities of a DimensionlessModel are retrieved explicitly and packed into a dictionary
-            raw_value = first(result.xpath('./value_phrase/value/text()'))
+            raw_value = first(result.xpath('./value/text()'))
             value = self.extract_value(raw_value)
             error = self.extract_error(raw_value)
             property_entities.update({"raw_value": raw_value,
@@ -161,8 +160,8 @@ class BaseAutoParser(BaseParser):
         elif hasattr(self.model, 'dimensions') and self.model.dimensions:
             # the specific entities of a QuantityModel are retrieved explicitly and packed into a dictionary
             # print(etree.tostring(result))
-            raw_value = first(result.xpath('./value_phrase/value/text()'))
-            raw_units = first(result.xpath('./value_phrase/units/text()'))
+            raw_value = first(result.xpath('./value/text()'))
+            raw_units = first(result.xpath('./units/text()'))
             value = self.extract_value(raw_value)
             error = self.extract_error(raw_value)
             units = None
