d77105c4966d7d90bb4bf0e8c75ced7330acd267
Taketomo <ti250@cam.ac.uk>
2019-09-13 16:09:06 +0100

    Add imports

--- chemdataextractor/parse/__init__.py
+++ chemdataextractor/parse/__init__.py
@@ -13,3 +13,5 @@ from .actions import join, merge, strip_stop, fix_whitespace
 from .elements import W, I, R, T, H
 from .elements import Any, Word, Tag, IWord, Regex, Start, End, Hide, Not
 from .elements import And, Or, First, ZeroOrMore, OneOrMore, Optional, Group, SkipTo
+from .auto import BaseAutoParser, AutoSentenceParser, AutoTableParser
+from .base import BaseParser, BaseSentenceParser, BaseTableParser
