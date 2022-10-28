f393d1ad0d56debbee2d06169f7302d6b57a1663
DINGYUN HUANG <94184902+Dingyun-Huang@users.noreply.github.com>
2022-10-28 09:19:28 +0100

    Updated required dependencies (#107)
    Specified the versions for boto3 and botocore to reduce installation time.

--- setup.py
+++ setup.py
@@ -28,6 +28,8 @@ setup(
     install_requires=[
         'appdirs',
         'beautifulsoup4',
+        'boto3==1.15.18',
+        'botocore==1.18.18',
         'click==6.7',
         'cssselect',
         'lxml',
