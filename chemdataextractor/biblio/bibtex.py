aadeb1f5c8f3f3f32292504e4cd54e679ba27757
Roma Klapaukh <rklapaukh@hotmail.com>
2023-04-05 14:02:42 +1000

    Revert incorrect fix

--- chemdataextractor/biblio/bibtex.py
+++ chemdataextractor/biblio/bibtex.py
@@ -8,7 +8,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
-from collections.abc import OrderedDict
+from collections import OrderedDict
 import json
 import re
 
